cfa44192502b0ce4ef61ae32cce2f0ed7ad51533
dponomarenko <ottosigma@gmail.com>
2024-04-20 01:09:43 +0200

    Temporary simplified downloader

--- src/downloader.rs
+++ src/downloader.rs
@@ -1,13 +1,9 @@
+use std::path::PathBuf;
 use rustube::{VideoFetcher, Result, VideoDescrambler};
 use rustube::url::Url;
 
-pub async fn download_video(url: &str) -> Result<VideoDescrambler> {
-    let url_obj = match url.parse::<Url>() {
-        Ok(url) => url,
-        Err(e) => return Err(e.into()),
-    };
+pub async fn download_video(url: &str) -> Result<PathBuf> {
+    let path_to_video = rustube::download_best_quality(url).await?;
 
-    let video_fetcher = VideoFetcher::from_url(&url_obj)?;
-    let video = video_fetcher.fetch().await?;
-    Ok(video)
+    Ok(path_to_video)
 }
--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ async fn main() {
     match downloader::download_video(&config.download_url).await {
         Ok(video) => {
             println!("Video downloaded successfully!");
-            video_processor::process_video("path_to_downloaded_video.mp4");
+            video_processor::process_video(video.to_str().unwrap());
         },
         Err(e) => println!("Failed to download video: {}", e),
     }
